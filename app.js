2ab7516eb55986581222fd1acc79337264eaf7aa
Murilo Henderson <acmurilohenderson@gmail.com>
2025-08-22 11:23:05 -0300

    Melhoria do jogo

--- app.js
+++ app.js
@@ -1,48 +1,64 @@
-let amigos = [];
-let amigosSorteados = [];
+    let amigos = [];
+    let amigosSorteados = [];
+    let amigosPresenteadores = [];
+    let amigosPresenteados = [];
 
-function atualizarLista() {
-    let lista = document.querySelector("#listaAmigos");
-    lista.innerHTML = "";
-
-    for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = amigos[i]; 
-        // Adicione o item i, ou seja, o nome que está alocado naquela posição dentro da array, dentro da tag "li"
-        lista.appendChild(li); 
-        // Coloca o item "li" dentro de <ul>
+    function adicionarAmigo() {
+        let amigo = document.querySelector("#amigo");
+        if (amigo.value === "") {
+            alert("Por favor, insira um nome.");
+        } else {
+            amigos.push(amigo.value);
+            amigosPresenteadores.push(amigo.value);
+            amigosPresenteados.push(amigo.value);
+            amigo.value = "";
+        }
+        atualizarLista();
     }
-}
 
-function adicionarAmigo() {
-    let amigo = document.querySelector("#amigo");
-    if (amigo.value === "") {
-        alert("Por favor, insira um nome.");
-    } else { 
-        amigos.push(amigo.value);
-        amigo.value = "";
+    function atualizarLista() {
+        let lista = document.querySelector("#listaAmigos");
+        lista.innerHTML = "";
+
+        for (let i = 0; i < amigos.length; i++) {
+            let li = document.createElement("li");
+            li.textContent = amigos[i]; 
+            // Adicione o item i, ou seja, o nome que está alocado naquela posição dentro da array, dentro da tag "li"
+            lista.appendChild(li); 
+            // Coloca o item "li" dentro de <ul>
+        }
     }
-    atualizarLista();
-}
 
-function sortearAmigo() {
-    if (amigos.length < 2) {
-        alert("Adicione ao menos dois amigos para fazer um sorteio.");
-    }
-    else {
-        let indicePresenteador = parseInt(Math.random() * amigos.length); 
-        let amigoPresenteador = amigos.splice(indicePresenteador, 1)[0];
+    function sortearAmigos() {
+        if (amigosPresenteadores.length == 0 || amigosPresenteados.length == 0) {
+            alert("Não existem amigos suficientes para fazer o sorteio");
+        } else {
+        let indicePresenteador = parseInt(Math.random() * amigosPresenteadores.length);
+        let amigoPresenteador = amigosPresenteadores.splice(indicePresenteador, 1)[0];    
 
-        let indicePresenteado = parseInt(Math.random() * amigos.length); 
-        let amigoPresentado = amigos.splice(indicePresenteado, 1)[0];
+        let indicePresenteado = parseInt(Math.random() * amigosPresenteados.length);
+        let amigoPresentado = amigosPresenteados.splice(indicePresenteado, 1)[0];
+        
+        if (amigosPresenteados.length > 1) {
+        while (amigoPresenteador === amigoPresentado) {
+                // Coloca o amigo de volta na lista e sorteia novamente
+                amigosPresenteados.push(amigoPresentado);
+                indicePresenteado = parseInt(Math.random() * amigosPresenteados.length);
+                amigoPresentado = amigosPresenteados.splice(indicePresenteado, 1)[0];
+            }
+        }
 
         amigosSorteados.push({presenteador: amigoPresenteador, presenteado: amigoPresentado});
 
- 
-        atualizarLista();
-
-        // mostrar o resultado
-        let resultado = document.querySelector("#resultado");
-        resultado.innerHTML = "O amigo " + amigoPresenteador + " deverá presentear o amigo " + amigoPresentado;
+        return {amigoPresenteador, amigoPresentado};
+        }
+       
     }
-}
+
+    function mostrarSorteio() { 
+        let sorteio = sortearAmigos();
+        if (sorteio) {
+            let resultado = document.querySelector("#resultado");
+            resultado.innerHTML = "O amigo " + sorteio.amigoPresenteador + " deverá presentear o amigo " + sorteio.amigoPresentado;
+        }
+    }
